3783933ac9c90a9a8a4c075ad23821c66ed1fb11
Vashcar Nath <vashcar21@gmail.com>
2026-07-01 23:34:24 -0400

    Add media item repository interface and implementation

---
+++ backend/src/MediaTracker.Data/IMediaItemRepository.cs
@@ -0,0 +1,13 @@
+using MediaTracker.Data.Models;
+
+namespace MediaTracker.Data
+{
+    public interface IMediaItemRepository
+    {
+        Task<List<MediaItem>> GetAllAsync();
+        Task<MediaItem?> GetByIdAsync(int id);
+        Task<MediaItem> AddItemAsync(MediaItem mediaItem);
+        Task<MediaItem> UpdateItemAsync(MediaItem mediaItem);
+        Task DeleteItemAsync(int id);
+    }
+}
---
+++ backend/src/MediaTracker.Data/MediaItemRepository.cs
@@ -0,0 +1,62 @@
+using Microsoft.EntityFrameworkCore;
+using MediaTracker.Data.Models;
+
+namespace MediaTracker.Data
+{
+    public class MediaItemRepository : IMediaItemRepository
+    {
+        private readonly MediaTrackerDbContext _mediaTrackerDbContext;
+        public MediaItemRepository(MediaTrackerDbContext dbContext)
+        {
+            _mediaTrackerDbContext = dbContext;
+        }
+
+        public async Task<List<MediaItem>> GetAllAsync()
+        {
+            List<MediaItem> mediaItemsList = await _mediaTrackerDbContext.MediaItems.AsNoTracking().ToListAsync();
+            return mediaItemsList;
+        }
+
+        public async Task<MediaItem?> GetByIdAsync(int id)
+        {
+            MediaItem? mediaItem = await _mediaTrackerDbContext.MediaItems
+                .AsNoTracking()
+                .FirstOrDefaultAsync(item => item.Id == id);
+
+            return mediaItem;
+        }
+
+        public async Task<MediaItem> AddItemAsync(MediaItem mediaItem)
+        {
+            await _mediaTrackerDbContext.AddAsync(mediaItem);
+            await _mediaTrackerDbContext.SaveChangesAsync();
+
+            return mediaItem;
+        }
+
+        public async Task<MediaItem> UpdateItemAsync(MediaItem updatedMediaItem)
+        {
+            MediaItem? existingMediaItem = await _mediaTrackerDbContext.MediaItems
+                .FirstOrDefaultAsync(item => item.Id == updatedMediaItem.Id);
+                
+            if (existingMediaItem is not null)
+            {
+                _mediaTrackerDbContext.Entry(existingMediaItem).CurrentValues.SetValues(updatedMediaItem);
+                await _mediaTrackerDbContext.SaveChangesAsync();
+            }
+
+            return updatedMediaItem;
+        }
+
+        public async Task DeleteItemAsync(int id)
+        {
+            MediaItem? itemToDelete = await _mediaTrackerDbContext.MediaItems.FindAsync(id);
+
+            if (itemToDelete is not null)
+            {
+                _mediaTrackerDbContext.MediaItems.Remove(itemToDelete);
+                await _mediaTrackerDbContext.SaveChangesAsync();
+            }
+        }
+    }
+}
