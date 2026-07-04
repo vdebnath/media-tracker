af02e3a031ec2f7645604b534bb1a28a79f27a8b
Vashcar Nath <vashcar21@gmail.com>
2026-07-04 15:35:49 -0400

    SCRUM 22: Add CORS config
    Added comments to Controller

--- backend/src/MediaTracker.Api/MediaItemsController.cs
+++ backend/src/MediaTracker.Api/MediaItemsController.cs
@@ -4,17 +4,28 @@ using MediaTracker.Data.Models;
 
 namespace MediaTracker.Api
 {
+    /// <summary>
+    /// Main controller for CRUD operations on media items 
+    /// </summary>
     [ApiController]
     [Route("api/[controller]")]
     public class MediaItemsController : ControllerBase
     {
         private readonly IMediaItemService _mediaItemService;
 
+        /// <summary>
+        /// Constructor for controller with service injected 
+        /// </summary>
+        /// <param name="service">Service interface being injected</param>
         public MediaItemsController(IMediaItemService service)
         {
             _mediaItemService = service;
         }
 
+        /// <summary>
+        /// Retrieves all media items
+        /// </summary>
+        /// <returns>List of all media items</returns>
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
@@ -22,6 +33,12 @@ namespace MediaTracker.Api
             return Ok(mediaItems);
         }
 
+        /// <summary>
+        /// Retrieves a single media item
+        /// </summary>
+        /// <param name="id">ID of media item to retrieve</param>
+        /// <returns>Single media item matching input ID</returns>
+        /// <remarks>404 error if no media item corresponds to input ID</remarks>
         [HttpGet("{id}")]
         public async Task<IActionResult> GetMediaItem(int id)
         {
@@ -35,6 +52,11 @@ namespace MediaTracker.Api
             return Ok(mediaItem);
         }
 
+        /// <summary>
+        /// Add a single media item to DB
+        /// </summary>
+        /// <param name="mediaItem">New media item to add</param>
+        /// <returns>201 created with media item that got added</returns>
         [HttpPost]
         public async Task<IActionResult> AddMediaItem(MediaItem mediaItem)
         {
@@ -42,6 +64,13 @@ namespace MediaTracker.Api
             return CreatedAtAction(nameof(GetMediaItem), new {id = addedMediaItem.Id}, addedMediaItem);
         }
 
+        /// <summary>
+        /// Edits a single media item 
+        /// </summary>
+        /// <param name="id">ID of media item to edit</param>
+        /// <param name="mediaItem">The details of the media item to be eddited</param>
+        /// <returns>200 ok with updated media item</returns>
+        /// <remarks>Return 401 if the input id does not match the media item</remarks>
         [HttpPut("{id}")]
         public async Task<IActionResult> UpdateMediaItem(int id, MediaItem mediaItem)
         {
@@ -55,12 +84,18 @@ namespace MediaTracker.Api
             return Ok(updatedMediaItem);
         }
 
+        /// <summary>
+        /// Removes a single media item from DB
+        /// </summary>
+        /// <param name="id">ID of media item to be removed</param>
+        /// <returns>204 No Content if successfull</returns>
+        /// <remarks>Returns 404 Not Found if input ID does not match any existing records</remarks>
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteMediaItem(int id)
         {
             bool gotDeleted = await _mediaItemService.DeleteItemAsync(id);
 
-            if(!gotDeleted)
+            if (!gotDeleted)
             {
                 return NotFound();
             }
--- backend/src/MediaTracker.Api/Program.cs
+++ backend/src/MediaTracker.Api/Program.cs
@@ -15,6 +15,17 @@ builder.Services.AddOpenApi();
 //Register Services for Business Logic
 builder.Services.RegisterServiceDependencies();
 
+//Add CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularDev", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 //Configure HTTP request pipeline
@@ -23,8 +34,10 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
 app.UseAuthorization();
+
+//Start up cors before mapping controllers
+app.UseCors("AllowAngularDev");
 
 app.MapControllers();
 
